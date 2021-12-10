0a54ff6083e98f2eb60290128bdd17aa6550450f
heyyou216 <95928292+heyyou216@users.noreply.github.com>
2021-12-10 10:53:50 -0500

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -1,7 +1,7 @@
 //  --> PLEASE READ THIS!!! <---
 //  
 //  Do not change the numbers in this file! Blooket has a hard limit on how many tokens
-//  you can add everyday. The limit is 500 and you cannot bypass it. Do not mess with
+//  you can add everyday. The limit is 5000 and you cannot bypass it. Do not mess with
 //  with this file if you do not know what you are doing!!!
 //
 
@@ -14,11 +14,11 @@ async function getName(authToken) {
 };
 
 async function addCurrencies() {
-    const tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
+    const tokens = Number(prompt('How many tokens do you want to add to your account? (5000 daily)'));
     const myToken = localStorage.token.split('JWT ')[1];
 
-    if (tokens > 500) {
-        alert('You can only add up to 500 tokens daily.')
+    if (tokens > 5000) {
+        alert('You can only add up to 5000 tokens daily.')
     }
 
     const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
@@ -30,13 +30,13 @@ async function addCurrencies() {
         },
         body: JSON.stringify({
             addedTokens: tokens,
-            addedXp: 300,
+            addedXp: 30000,
             name: await getName(myToken)
         })
     });
 
     if (response.status == 200) {
-        alert(`${tokens} tokens and 300 XP added to your account!`);
+        alert(`${tokens} tokens and 30000 XP added to your account!`);
     } else {
         alert('An error occured.');
     };
